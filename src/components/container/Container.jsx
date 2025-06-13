2750d404cfe1cbb8d98c34f93d04765bd5c3e1f6
user.name "chaitanyshinde2323@gmail.com <chaitanyshinde2323@gmail.com>
2025-06-13 19:28:39 +0530

    Header

---
+++ src/components/container/Container.jsx
@@ -0,0 +1,12 @@
+//excepts properties as children:
+//there will be something at top and bottom of container. 
+
+import React from 'react'
+
+function Container({children}) {
+  return (
+    <div className='w-full max-w-7xl mx-auto px-4'>{children}</div>
+  )
+}
+
+export default Container
